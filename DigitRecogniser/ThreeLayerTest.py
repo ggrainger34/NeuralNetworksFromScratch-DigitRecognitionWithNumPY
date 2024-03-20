79489666d15ab520d2d7618e8e0f9ce2548a0672
ggrainger34 <ggrainger34@gmail.com>
2024-03-20 16:09:49 +0000

    V1

--- DigitRecogniser/ThreeLayerTest.py
+++ DigitRecogniser/ThreeLayerTest.py
@@ -1,29 +1,21 @@
 import numpy as np
 
 class Layer:
-    def __init__(self, prevLayer, activations, weights, neuronCount):
-        self.weights = weights
+    def __init__(self, inputCount, neuronCount):
+        self.inputs = inputCount
         self.neuronCount = neuronCount
-        self.prevLayer = prevLayer
-        self.activations = activations
-
-    #Forward pass calculates
-    def forward(self):
-        if self.prevLayer == None:
-            return self.activations
-        return self.activationFunction(self.weights, self.prevLayer.activations)
-
-    #Activation Function takes in the weights between the previous layer and the current layer as a matrix
-    #And multiplies it with the activations of the previous layer
-    def activationFunction(self, weights, prevLayerActivations):
-        return weights.dot(prevLayerActivations)
+        self.weights = np.random.randn(inputCount, neuronCount)
+    
+    def forward(self, inputs):
+        return np.dot(inputs, self.weights)
 
 def main():
-    a0 = Layer(None, np.array([0.3, 0.2, 0.4]), None, 3)
-    a1 = Layer(a0, None, np.array([[1,1,1], [1,1,1]]), 2)
+    input = [0.2, 0.3, 0.4]
 
-    print(a0.forward())
-    print(a1.forward())
+    a0 = Layer(3,2)
+    a1 = Layer(2,2)
+
+    print(a0.forward(input))
 
 if __name__ == '__main__':
     main()
